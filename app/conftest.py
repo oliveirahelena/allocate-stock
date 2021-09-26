23a4a98a61c74ee7c72745155fbecc31d24f085f
Helena Oliveira <helena@informach.com.br>
2021-09-26 14:09:49 -0300

    Repository Pattern

---
+++ app/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from orm import metadata, start_mappers
+from sqlalchemy import create_engine
+from sqlalchemy.orm import clear_mappers, sessionmaker
+
+
+@pytest.fixture
+def in_memory_db():
+    engine = create_engine("sqlite:///:memory:")
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def session(in_memory_db):
+    start_mappers()
+    yield sessionmaker(bind=in_memory_db)()
+    clear_mappers()
